18bdf0c3a48f0fbc876b08583fef9dbb412553a1
SirMathhman <sirmathhman@gmail.com>
2024-02-20 17:14:39 -0500

    Print out lines

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import Koa from "koa";
 import {promises as fs} from "fs";
+import cors from "@koa/cors";
 
 const app = new Koa();
+app.use(cors());
+
 app.use(async ctx => {
     if (ctx.path === '/list') {
         try {
-            const files = await fs.readdir(process.cwd());
-            ctx.body = files.join('\n');
+            ctx.body = await fs.readdir(process.cwd());
         } catch (err) {
             ctx.status = 500;
             ctx.body = 'Error reading directory';
