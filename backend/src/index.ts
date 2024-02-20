e222c3e8dd7c2fb980eee813eb7a27d7547fab0a
SirMathhman <sirmathhman@gmail.com>
2024-02-20 18:34:27 -0500

    Extract subroutines

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,45 +1,50 @@
-import Koa from "koa";
+import Koa, {ParameterizedContext} from "koa";
 import {promises as fs} from "fs";
 import cors from "@koa/cors";
 import Router from "koa-router";
 
-function main() {
-    const app = new Koa();
-    app.use(cors());
+async function readFiles(context: ParameterizedContext) {
+    try {
+        context.body = await fs.readdir(process.cwd());
+    } catch (err) {
+        context.status = 500;
+        context.body = 'Error reading directory';
+    }
+}
 
+async function readChild(context: ParameterizedContext) {
+    const params = context.params;
+    const child = params["child"];
+
+    if (!child) {
+        context.status = 400;
+        context.body = "No child provided.";
+        return;
+    }
+
+    try {
+        const output = await fs.readFile(child);
+        context.status = 200;
+        context.body = output;
+    } catch (e) {
+        context.status = 500;
+        context.message = `Failed to read file at '${child}'.`;
+    }
+}
+
+function createFileRoutes() {
     const router = new Router({
         prefix: "/file"
     });
+    router.get("/", readFiles);
+    router.get("/:child", readChild);
+    return router;
+}
 
-    router.get("/", async context => {
-        try {
-            context.body = await fs.readdir(process.cwd());
-        } catch (err) {
-            context.status = 500;
-            context.body = 'Error reading directory';
-        }
-    });
-
-    router.get("/:child", async context => {
-        const params = context.params;
-        const child = params["child"];
-
-        if (!child) {
-            context.status = 400;
-            context.body = "No child provided.";
-            return;
-        }
-
-        try {
-            const output = await fs.readFile(child);
-            context.status = 200;
-            context.body = output;
-        } catch (e) {
-            context.status = 500;
-            context.message = `Failed to read file at '${child}'.`;
-        }
-    });
-    app.use(router.routes());
+function main() {
+    const app = new Koa();
+    app.use(cors());
+    app.use(createFileRoutes().routes());
 
     app.listen(3000, () => {
         console.log('Server running on http://localhost:3000');
