f9f3550c113cb3e92bb9d661679bd4358083223f
SirMathhman <sirmathhman@gmail.com>
2024-03-07 23:28:19 -0500

    OK

--- backend/test/index.test.ts
+++ backend/test/index.test.ts
@@ -1,6 +1,6 @@
 import * as assert from "assert";
 import axios from "axios";
-import Koa from "koa";
+import Koa, {Context} from "koa";
 
 describe('Jest', () => {
     it('should work fine', () => {
@@ -28,7 +28,7 @@ const PORT = 3000;
 const EXPECTED = "test";
 describe('Within an integration context, a Koa server', () => {
     it('should be able to be connectable', async () => {
-        const app = new Koa().use(context => {
+        const app = new Koa().use(function (context: Context) {
             context.response.body = EXPECTED;
             context.response.status = 200;
         });
