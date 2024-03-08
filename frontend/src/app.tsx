d1d22b838396f52765dbd683d4ae21e7d38e23e4
SirMathhman <sirmathhman@gmail.com>
2024-03-07 23:53:19 -0500

    Add navigation text

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,23 +1,55 @@
-import {component$} from '@builder.io/qwik'
+import {component$, Slot} from '@builder.io/qwik';
+
+interface TextProps {
+}
+
+export const Text = component$<TextProps>(() => {
+    return (
+        <span style={{
+            "font-family": "Arial"
+        }}>
+            <Slot/>
+        </span>
+    )
+});
+
+interface PaddingProps {
+}
+
+export const Padding = component$<PaddingProps>(() => {
+    return (
+        <div style={{
+            padding: "0.5rem"
+        }}>
+            <Slot/>
+        </div>
+    )
+});
 
 export const App = component$(() => {
     return (
-        <div>
-            <div style={{
-                padding: "0.5rem"
-            }}>
-            <span style={{
-                "font-family": "Arial",
-            }}>
-                File
-            </span>
-            </div>
+        <div style={{
+            display: "flex",
+            "flex-direction": "column"
+        }}>
+            <Padding>
+                <Text>
+                    File
+                </Text>
+            </Padding>
             <hr style={{
                 padding: 0,
                 margin: 0
             }}/>
-            <div>
-
+            <div style={{
+                display: "flex",
+                "flex-direction": "row"
+            }}>
+                <Padding>
+                    <Text>
+                        Navigation
+                    </Text>
+                </Padding>
             </div>
         </div>
     )
