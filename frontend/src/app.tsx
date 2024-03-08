ef226bfde00799cfdd1588b7776b8de2cadfc9b3
SirMathhman <sirmathhman@gmail.com>
2024-03-07 23:36:24 -0500

    Add HR

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,14 +2,23 @@ import {component$} from '@builder.io/qwik'
 
 export const App = component$(() => {
     return (
-        <div style={{
-            padding: "0.5rem"
-        }}>
+        <div>
+            <div style={{
+                padding: "0.5rem"
+            }}>
             <span style={{
                 "font-family": "Arial",
             }}>
                 File
             </span>
+            </div>
+            <hr style={{
+                padding: 0,
+                margin: 0
+            }}/>
+            <div>
+
+            </div>
         </div>
     )
 })
