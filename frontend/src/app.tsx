2b0413b9964a28d9c1aaa2532f49c5b2f787ee05
SirMathhman <sirmathhman@gmail.com>
2024-03-08 00:04:14 -0500

    Add constraints

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -26,31 +26,81 @@ export const Padding = component$<PaddingProps>(() => {
     )
 });
 
-export const App = component$(() => {
+export interface FlexProps {
+    direction: "row" | "column";
+}
+
+export const Flex = component$<FlexProps>(({direction}) => {
     return (
         <div style={{
             display: "flex",
-            "flex-direction": "column"
+            "flex-direction": direction,
+            width: "100%",
+            height: "100%"
         }}>
+            <Slot/>
+        </div>
+    )
+});
+
+export const Row = component$<Omit<FlexProps, "direction">>(props => {
+    return <Flex {...props} direction="row"><Slot/></Flex>
+});
+
+export const Column = component$<Omit<FlexProps, "direction">>(props => {
+    return <Flex {...props} direction="column"><Slot/></Flex>
+});
+
+export const HorizontalRule = component$(() => {
+    return (<hr style={{
+        padding: 0,
+        margin: 0
+    }}/>)
+});
+
+export interface ConstraintProps {
+    expanded?: boolean;
+    height?: string;
+    width?: string;
+}
+
+export const Constraint = component$<ConstraintProps>(({width, height, expanded}) => {
+    return (
+        <div style={{
+            width: width ?? (expanded ? "100%" : undefined),
+            height: height ?? (expanded ? "100%" : undefined)
+        }}>
+            <Slot/>
+        </div>
+    );
+});
+
+export const App = component$(() => {
+    return (
+        <Column>
             <Padding>
                 <Text>
                     File
                 </Text>
             </Padding>
-            <hr style={{
-                padding: 0,
-                margin: 0
-            }}/>
-            <div style={{
-                display: "flex",
-                "flex-direction": "row"
-            }}>
-                <Padding>
-                    <Text>
-                        Navigation
-                    </Text>
-                </Padding>
-            </div>
-        </div>
+            <HorizontalRule/>
+            <Row>
+                <Constraint width="20%" expanded>
+                    <Padding>
+                        <Text>
+                            Navigation
+                        </Text>
+                    </Padding>
+                </Constraint>
+                <HorizontalRule/>
+                <Constraint width="80%" expanded>
+                    <Padding>
+                        <Text>
+                            Content
+                        </Text>
+                    </Padding>
+                </Constraint>
+            </Row>
+        </Column>
     )
 })
