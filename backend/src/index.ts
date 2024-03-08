768110a3dcc3f70f735d7470f20523fcee94f42d
SirMathhman <sirmathhman@gmail.com>
2024-03-08 00:22:34 -0500

    Pull port from .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import Koa from "koa";
 import Application from "koa";
 import {Once} from "./once";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export interface DefaultApplication extends Application<Application.DefaultState, Application.DefaultContext> {
 }
@@ -11,8 +14,29 @@ export function run(port: number, setup: (koa: DefaultApplication) => DefaultApp
     return Once(app.listen(port), server => server.close());
 }
 
+const DEFAULT_PORT = 3000;
+
+function findPort() {
+    const port = process.env["PORT"];
+    if (port) {
+        try {
+            let parsedPort = parseInt(port);
+            console.log(`Using user specified port of '${port}'.`);
+            return parsedPort;
+        } catch (e) {
+            console.warn(`Failed to parse user specified port of '${port}'.`);
+            console.log("Using default port of " + DEFAULT_PORT);
+            return DEFAULT_PORT;
+        }
+    } else {
+        console.log("Using default port of " + DEFAULT_PORT);
+        return DEFAULT_PORT;
+    }
+}
+
 async function main() {
-    run(3000, app => {
+    let actualPort = findPort();
+    run(actualPort, app => {
         return app;
     });
 }
