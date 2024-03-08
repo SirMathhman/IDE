ac6b56ffbf3c0c1cd1b4ac22f4a7e9927974c152
SirMathhman <sirmathhman@gmail.com>
2024-03-07 23:35:34 -0500

    Add File text

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,8 +2,14 @@ import {component$} from '@builder.io/qwik'
 
 export const App = component$(() => {
     return (
-        <div>
-            Test
+        <div style={{
+            padding: "0.5rem"
+        }}>
+            <span style={{
+                "font-family": "Arial",
+            }}>
+                File
+            </span>
         </div>
     )
 })
